4fe945305e5918515bd047feed465986a36ca0f6
Vicky Kumar <imvickykumar999@gmail.com>
2024-08-06 16:08:27 +0530

    better css

--- blog/views.py
+++ blog/views.py
@@ -66,7 +66,7 @@ def add_comment(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment.html', {'form': form})
+    return render(request, 'blog/add_comment.html', {'form': form, 'post' : post})
 
 @login_required
 def like_dislike_post(request, pk, is_like):
